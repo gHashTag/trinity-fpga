2da0228711177487b63332f89b207e89b5fafcc9
Dmitrii Vasilev <admin@t27.ai>
2026-09-09 10:41:45 +0000

    gates: seven caption self-references dropped; wide sweep harness fixed (takum16 clock was tied to 1'b0, tnf64s x-select was out of range)

--- fpga/tnet/conform_wide.sh
+++ fpga/tnet/conform_wide.sh
@@ -2,10 +2,11 @@
 # The wide formats, sampled. A 32- or 64-bit sweep cannot be exhaustive, but an
 # undefined output does not hide: a pseudorandom walk over 40,000 codes finds an
 # X module as surely as an exhaustive one, and that is what this check is for.
-run() {  # $1 name  $2 width  $3 instance  $4 sources
+run() {  # $1 name  $2 width  $3 instance  $4 sources  [$5 x-expression]
 cat > tw_$1.v <<V
 \`timescale 1ns/1ps
 module tw_$1;
+  reg clk = 0; always #1 clk = ~clk;
   reg [$(($2-1)):0] x; wire [31:0] fp;
   $3
   integer i; reg [63:0] r;
@@ -13,7 +14,7 @@ module tw_$1;
     r = 64'h2545F4914F6CDD1D;
     for (i = 0; i < 40000; i = i + 1) begin
       r = r * 64'd6364136223846793005 + 64'd1442695040888963407;
-      x = r[$(($2-1)):0]; #1; \$display("%0d %08x", x, fp);
+      x = ${5:-r}; @(posedge clk); #1; \$display("%0d %08x", x, fp);
     end
     \$finish;
   end
@@ -27,5 +28,5 @@ run vaxf     32 "vax_f_decode dec (.vax_in(x), .fp32_out(fp));" "vax_f_decode.v"
 run ibmhfp   32 "ibm_hfp32_decode dec (.ibm_in(x), .fp32_out(fp));" "ibm_hfp32_decode.v"
 run posit32  32 "posit32_decode dec (.posit_in(x), .fp32_out(fp), .is_zero(), .is_nar());" "posit32_decode.v"
 run posit8    8 "posit8_es2_decode dec (.posit_in(x), .fp32_out(fp), .is_zero(), .is_nar());" "posit8_es2_decode.v posit16_decode.v"
-run takum16  16 "takum16_decode dec (.clk(1'b0), .takum16_in(x), .fp32_out(fp));" "takum16_decode.v"
-run tnf64s   65 "tnf64s_decode dec (.x(x), .fp32_out(fp));" "tnf_spec_decode.v"
+run takum16  16 "takum16_decode dec (.clk(clk), .takum16_in(x), .fp32_out(fp));" "takum16_decode.v"
+run tnf64s   65 "tnf64s_decode dec (.x(x), .fp32_out(fp));" "tnf_spec_decode.v" "{r[63], r}"
